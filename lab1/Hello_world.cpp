123090b678021a1960d68766858116f5cf6bb536
Nazyrick <ulasevich_nazar@mail.ru>
2020-03-29 23:09:03 +0300

    the first laboratory

---
+++ lab1/Hello_world.cpp
@@ -0,0 +1,15 @@
+﻿#include <stdlib.h>
+#include <tchar.h>
+#include <iostream>
+#include <stdio.h>
+#include <sstream>
+
+using namespace std;
+
+
+int _tmain(int argc, _TCHAR argv[])
+{
+	std::cout << "Hello World!\n";
+	system("pause");
+	return 0;
+}
