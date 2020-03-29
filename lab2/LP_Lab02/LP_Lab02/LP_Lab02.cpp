c2e20fdf6542b45b39d40a73617397aff95120c1
Nazyrick <ulasevich_nazar@mail.ru>
2020-03-29 23:21:44 +0300

    the second laboratory

---
+++ lab2/LP_Lab02/LP_Lab02/LP_Lab02.cpp
@@ -0,0 +1,10 @@
+﻿#include "stdafx.h"
+
+int main(int argc, _TCHAR* ARGV[])
+{
+	std::cout << "sum(2,3) = " << sum(2, 3) << std::endl;
+	std::cout << "sub(2,3) = " << sub(2, 3) << std::endl;
+	std::cout << "mul(2,3) = " << mul(2, 3) << std::endl;
+	system("pause");
+	return 0;
+}
---
+++ lab2/LP_Lab02/LP_Lab02/stdafx.h
@@ -0,0 +1,8 @@
+#pragma once
+#include "stdio.h"
+#include "tchar.h"
+#include "stdlib.h"
+#include <iostream>
+int sum(int x, int  y);
+int sub(int x, int y);
+int mul(int x, int y);
